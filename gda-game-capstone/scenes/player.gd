0d74bce29ccecd2bfb78e486d78bd03020736294
hatguy33 <maderas@s.sfusd.edu>
2025-03-06 23:43:39 -0800

    simple dash

--- gda-game-capstone/scenes/player.gd
+++ gda-game-capstone/scenes/player.gd
@@ -1,30 +1,37 @@
 extends CharacterBody2D
 
-var speed := 100
-@export var acceleration := 1200.0
-@export var deceleration := 1080.0
-const dash = 500
-var tween: Tween
+var speed := 300
+@export var acceleration := 10000
+@export var deceleration := 10000
+@export var dash_speed := 1500.0
+@export var dash_duration := .2
+var is_dashing := false
+var dash_timer := 0.0
+var dash_direction := Vector2.ZERO
 var dash_velocity = 0.0
 func _ready() -> void:
 	pass # Replace with function body.
-func dashh():
-	if Input.is_action_just_pressed("dash"):
-		dash_velocity = dash
-		if tween:
-			tween.stop()
-		tween = create_tween()
-		tween.tween_property(self, "dash_velocity", 0, 0.2).set_ease(Tween.EASE_OUT)
+
 
 
 func _physics_process(delta: float) -> void:
-	var direction := Input.get_vector("left", "right", "up", "down")
-	var has_input_direction := direction.length() > 0.0
-	if has_input_direction:
-		var desired_velocity := direction * speed
-		velocity = velocity.move_toward(desired_velocity, acceleration * delta)
+	if is_dashing:
+		dash_timer -= delta
+		if dash_timer <= 0:
+			is_dashing = false
+		velocity = dash_direction * dash_speed
 	else:
-		velocity = velocity.move_toward(Vector2.ZERO, deceleration * delta)
+		var direction := Input.get_vector("left", "right", "up", "down")
+		var has_input_direction := direction.length() > 0.0
+		if has_input_direction:
+			var desired_velocity := direction * speed
+			velocity = velocity.move_toward(desired_velocity, acceleration * delta)
+		else:
+			velocity = velocity.move_toward(Vector2.ZERO, deceleration * delta)
+		if Input.is_action_pressed("dash") and has_input_direction:
+			start_dash(direction)
 	move_and_slide()
-	if input_axis:
-		move_and_slide()
+func start_dash(direction: Vector2):
+	is_dashing = true
+	dash_timer = dash_duration
+	dash_direction = direction
