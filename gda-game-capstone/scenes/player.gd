33c5f708855efb5e67e4753702212254cff13aca
hatguy33 <maderas@s.sfusd.edu>
2025-03-07 15:38:08 -0800

    dash cooldown

--- gda-game-capstone/scenes/player.gd
+++ gda-game-capstone/scenes/player.gd
@@ -5,12 +5,16 @@ var speed := 300
 @export var deceleration := 10000
 @export var dash_speed := 1500.0
 @export var dash_duration := .2
+@export var dash_cooldown := 1.5
+
 var is_dashing := false
 var dash_timer := 0.0
 var dash_direction := Vector2.ZERO
 var dash_velocity = 0.0
+@onready var cooldown_timer = $Cooldown
 func _ready() -> void:
-	pass # Replace with function body.
+	cooldown_timer.wait_time = dash_cooldown
+	cooldown_timer.one_shot = true
 
 
 
@@ -19,6 +23,7 @@ func _physics_process(delta: float) -> void:
 		dash_timer -= delta
 		if dash_timer <= 0:
 			is_dashing = false
+			cooldown_timer.start()
 		velocity = dash_direction * dash_speed
 	else:
 		var direction := Input.get_vector("left", "right", "up", "down")
@@ -28,10 +33,13 @@ func _physics_process(delta: float) -> void:
 			velocity = velocity.move_toward(desired_velocity, acceleration * delta)
 		else:
 			velocity = velocity.move_toward(Vector2.ZERO, deceleration * delta)
-		if Input.is_action_pressed("dash") and has_input_direction:
+		if Input.is_action_just_pressed("dash") and has_input_direction and cooldown_timer.is_stopped():
 			start_dash(direction)
+
+
 	move_and_slide()
 func start_dash(direction: Vector2):
 	is_dashing = true
 	dash_timer = dash_duration
 	dash_direction = direction
+	cooldown_timer.stop()
