afec10f17fd7ba44d58e538cbc5fea8c55d95944
hatguy33 <maderas@s.sfusd.edu>
2025-03-24 15:39:41 -0700

    sword animation + new player sprite

--- gda-game-capstone/scripts/sword.gd
+++ gda-game-capstone/scripts/sword.gd
@@ -1,17 +1,18 @@
 extends Node2D
 class_name Sword
 
+@onready var slashh: AnimatedSprite2D = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
-
+	if Input.is_action_just_pressed("Swing"):
+		slashh.play("swingg")
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Mob:
 		body.take_damage(5)
