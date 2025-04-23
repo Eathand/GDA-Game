b24d6797e146167e3090f80ad1d153636534f5e2
hatguy33 <maderas@s.sfusd.edu>
2025-04-23 13:07:52 -0700

    added sfx to weapon

--- gda-game-capstone/scripts/sword.gd
+++ gda-game-capstone/scripts/sword.gd
@@ -1,5 +1,8 @@
 extends Node2D
 class_name Sword
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
+@onready var collision_shape_2d: CollisionShape2D = $Area2D/CollisionShape2D
+@onready var audio_stream_player_2d_2: AudioStreamPlayer2D = $AudioStreamPlayer2D2
 
 @onready var slashh: AnimatedSprite2D = $AnimatedSprite2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
@@ -18,10 +21,11 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Mob and (area_2d.visible == true):
 		body.take_damage(5)
 		print(body.current_health)
-		
+		audio_stream_player_2d.play()
 
 func swing():
 	if Input.is_action_just_pressed("Swing"):
 		slashh.play("swingg")
 		animation_player.play("swing")
+		audio_stream_player_2d_2.play()
 		await animation_player
