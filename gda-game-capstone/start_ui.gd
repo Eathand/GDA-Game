8cbc8c6db5c6e5f04f4cd7406c725bca832b1543
Gaymer7750 <159182541+Gaymer7750@users.noreply.github.com>
2025-05-09 15:39:12 -0700

    Transition fixes

--- gda-game-capstone/start_ui.gd
+++ gda-game-capstone/start_ui.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://realtest.tscn")
+	Transition.load_scene("res://realtest.tscn")
 
 
 func _on_quit_button_pressed() -> void:
