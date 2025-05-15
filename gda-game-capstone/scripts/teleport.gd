48c8f2868840bef91425ec2dfd685e9c8915be72
hatguy33 <maderas@s.sfusd.edu>
2025-05-14 23:44:20 -0700

    audio + tp fixed

--- gda-game-capstone/scripts/teleport.gd
+++ gda-game-capstone/scripts/teleport.gd
@@ -3,9 +3,15 @@ extends Area2D
 @export var new_position_x: int
 @export var new_position_y: int
 
+@onready var regular_bgm: AudioStreamPlayer2D = $"../Player/RegularBGM"
+@onready var calm_bgm: AudioStreamPlayer2D = $"../Player/CalmBGM"
+@onready var boss_bgm: AudioStreamPlayer2D = $"../Player/BossBGM"
+
+
+@export var song_type: AudioStreamPlayer2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,3 +22,10 @@ func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
 		body.position.x = new_position_x
 		body.position.y = new_position_y
+	print(song_type)
+	if song_type == null: pass 
+	else:
+		regular_bgm.stop()
+		calm_bgm.stop()
+		boss_bgm.stop()
+		song_type.play()
